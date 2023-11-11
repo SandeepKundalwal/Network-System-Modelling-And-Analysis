69076dcffe606dd66daaa4cf5bf41173e9e3d910
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2023-11-11 18:37:18 +0530

    adding Assignment-3/Code/Q2-b.py

---
+++ LabAssignments/Assignment-3/Code/Q2-b.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def calculate_steady_state(A, b):
+    n = A.shape[0]
+    I = np.identity(n)
+    x = np.linalg.inv(I - A) @ b
+    return x
+
+def main():
+    A = np.array([[0.2, 0.3, 0.7],
+                [0.3, 0.1, 0.7],
+                [0.7, 0, 0]])
+    b = np.array([0, 20, 80])
+    steady_state_x = calculate_steady_state(A, b)
+    print("Steady state x:", steady_state_x)
+    # print(steady_state_x)
+
+if __name__ == '__main__':
+    main()
