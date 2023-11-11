ea01c0e643b6cba66f85e752ce09c0c9e50d8a82
SandeepKundalwal <sandeepkundalwal96@gmail.com>
2023-11-11 18:42:56 +0530

    updating Assignment-3/Code/Q6.py

--- LabAssignments/Assignment-3/Code/Q6.py
+++ LabAssignments/Assignment-3/Code/Q6.py
@@ -22,21 +22,25 @@ class QueueSystem:
             yield self.env.timeout(1)
             self.x2 = max(0, self.x2 + dx2)
 
-# parameters
-lambda1 = 2.0
-lambda2 = 1.0
-mu1 = 3.0
-mu2 = 2.0
-sim_duration = 1000
+def main():
+    # parameters
+    lambda1 = 2.0
+    lambda2 = 1.0
+    mu1 = 3.0
+    mu2 = 2.0
+    sim_duration = 1000
 
-# simulation environment
-env = simpy.Environment()
-queue_system = QueueSystem(env, lambda1, lambda2, mu1, mu2)
-env.process(queue_system.update_queue1())
-env.process(queue_system.update_queue2())
-env.run(until=sim_duration)
-equilibrium_x1 = (lambda1 / mu1) / (1 - (lambda1 / mu1))
-equilibrium_x2 = (lambda2 / mu2) / (1 - (lambda2 / mu2))
+    # simulation environment
+    env = simpy.Environment()
+    queue_system = QueueSystem(env, lambda1, lambda2, mu1, mu2)
+    env.process(queue_system.update_queue1())
+    env.process(queue_system.update_queue2())
+    env.run(until=sim_duration)
+    equilibrium_x1 = (lambda1 / mu1) / (1 - (lambda1 / mu1))
+    equilibrium_x2 = (lambda2 / mu2) / (1 - (lambda2 / mu2))
 
-print(f"Equilibrium Queue Size for Queue 1: {equilibrium_x1:.2f}")
-print(f"Equilibrium Queue Size for Queue 2: {equilibrium_x2:.2f}")
+    print(f"Equilibrium Queue Size for Queue 1: {equilibrium_x1:.2f}")
+    print(f"Equilibrium Queue Size for Queue 2: {equilibrium_x2:.2f}")
+
+if __name__ == '__main__':
+    main()
